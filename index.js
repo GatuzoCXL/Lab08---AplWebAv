ac7023794301a0706e457776ab4517fd325c0649
GatuzoCXL <tkmlc29@gmail.com>
2025-05-05 12:37:09 -0500

    Añadido script start y mejoras para despliegue en Render

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 require('dotenv').config()
 const http= require('http')
 
-function requestController(){
+function requestController(req, res){
     console.log('Bienvenidos al curso de Mandarinas')
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('<h1>¡Aplicación desplegada en Render exitosamente!</h1>');
 }
 
 const server=http.createServer(requestController)
 
-const PORT=process.env.PORT
+const PORT=process.env.PORT || 3000
 
 server.listen(PORT, function(){
     console.log("Aplicacion corriendo en: " + PORT)
